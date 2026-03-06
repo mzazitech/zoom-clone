a410cafa8cb2b9a7fb203a3b96c7b4a2d400d3fd
Mzazi-tech <mzazitech001@gmail.com>
2026-03-06 07:32:14 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ import { Toaster } from "@/components/ui/toaster";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "YOOM",
+  title: "MZAZI TECH INC MEETING",
   description: "Video calling App",
   icons: {
     icon: "/icons/logo.svg",
